c71ef955e394c9a175b6c9735b19ac2070e1cdec
Grego Corpas <gregocorpas89@yahoo.es>
2014-11-20 19:58:53 +0100

    practica 3

---
+++ practica3/ejercicio3.c
@@ -0,0 +1,81 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+pthread_mutex_t semaforo;//Semaforo
+
+int nElementos=5;
+int buffer[5];//Vector
+int n=0;//Elementos utiles dentro del vector
+
+void * productor(void * arg){
+
+int posEscribir=0;//Posicion donde comienza a escribir
+
+	while(1){//While TRUE
+		while(n==nElementos);//Espera activa mientras el buffer este lleno
+	pthread_mutex_lock(&semaforo);
+	//Seccion critica
+	
+	buffer[posEscribir]=rand()%10;
+	//imprimir que he generado y en que posicion
+
+//*****************
+	posEscribir= (posEscribir+1)%nElementos;//Metemos un numero aleatorio
+	
+	sleep(posEscribir);
+	//Fin seccion critica
+	pthread_mutex_unlock(&semaforo);	
+	}
+pthread_exit(NULL);
+}
+
+void * consumidor(void * arg){
+
+int posLeer=0;//Posicion donde comienza a leer
+	
+	while(1){//While TRUE
+		while(n==0);//Espera activa mientras el buffer este vacío	
+	//Seccion critica	
+	pthread_mutex_lock(&semaforo);
+	fflush(stdout);
+	system("clear");
+
+	//buffer[posLeer]=rand()%10;
+
+	posLeer= (posLeer+1)%nElementos;//Metemos un numero aleatorio
+
+//imprimir que he consumido y en que posicion
+//**********
+	printf("%d",buffer[(posLeer+1)%nElementos]);
+
+	posLeer+=1;//El indice por el que vamos leyendo se actualiza
+
+	n-=1;//Un elemento util menos en el buffer
+	sleep(posLeer);
+	pthread_mutex_unlock(&semaforo);
+	//Fin de seccion critica
+	}
+pthread_exit(NULL);
+}
+
+int main(){
+
+srand(time(NULL));
+
+pthread_mutex_init(&semaforo,NULL);
+pthread_t * hilos;
+
+hilos=(pthread_t *)malloc(sizeof(pthread_t)*2);
+
+pthread_create(&hilos[0],NULL,(void *)productor,NULL);
+pthread_create(&hilos[1],NULL,(void *)consumidor,NULL);
+
+pthread_join(hilos[0],NULL);
+pthread_join(hilos[1],NULL);
+
+return 0;
+}
+
+
